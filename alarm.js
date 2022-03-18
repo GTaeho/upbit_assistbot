328531877b4ab6662a64997ad8c0a6d736367a63
GTaeho <prid77@naver.com>
2022-03-18 20:48:15 +0900

    기술적지표 추가

--- alarm.js
+++ alarm.js
@@ -8,16 +8,36 @@
   ** Github repository : https://github.com/GTaeho/upbit_assistbot
 */
 
+import ta from "technicalindicators";
+import { krwbtc_raw_sample_data } from "./upbit.js";
+
 export const startAlarm = () => {
   let prev_minute = 99;
-  setInterval(() => {
+  setInterval(async () => {
     const date = new Date();
     const minute_now = date.getMinutes();
 
     // 매 분마다 실행
     if (minute_now !== prev_minute) {
       prev_minute = minute_now;
-      console.log("매 1분 마크 : " + minute_now);
+
+      // 업비트에서 KRW-BTC 가격가져오기
+      const data = await raw_sample_data();
+      const macdInput = {
+        values: data,
+        fastPeriod: 12,
+        slowPeriod: 26,
+        signalPeriod: 9,
+        SimpleMAOscillator: false,
+        SimpleMASignal: false,
+      };
+      let result = ta.macd(macdInput);
+      console.log(result[result.length - 5]);
+      console.log(result[result.length - 4]);
+      console.log(result[result.length - 3]);
+      console.log(result[result.length - 2]);
+      console.log(result[result.length - 1]);
+      console.log("-------------------------");
 
       // 매 3분마다 실행
       if (minute_now % 3 === 0) {
--- upbit.js
+++ upbit.js
@@ -10,6 +10,25 @@
 
 import fetch from "node-fetch";
 
+export const raw_sample_data = async () => {
+  /**
+   * 주의할 점은, 한 번에 200개 까지의 캔들을 요청할 수 있으며 이를 초과하는 경우
+   * 페이지네이션 기술을 이용해 순차적으로 요청하는 것을 권장합니다.
+   * 뭘 계산하던지 200개면 보통 실시간 최신값을 받는데는 크게 문제없다.
+   * DB에 저장하고 할 필요는 아직 없음.
+   */
+  const res = await fetch(
+    "https://api.upbit.com/v1/candles/minutes/5?market=KRW-BTC&count=200"
+  );
+  const data = await res.json();
+  const sampleDataArray = [];
+  for (let key in data) {
+    sampleDataArray.push(data[key]["trade_price"]);
+  }
+  // return sampleDataArray
+  return sampleDataArray.reverse();
+};
+
 export const krwbtc_sample_data = async () => {
   /**
    * 주의할 점은, 한 번에 200개 까지의 캔들을 요청할 수 있으며 이를 초과하는 경우
